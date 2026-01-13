a46ffd704d765fabd3e9cf2f1972598b4dbc651c
The Harry Code <theharrycode@gmail.com>
2026-01-13 11:03:20 -0500

    change

--- src/main/java/com/lombok/service/impl/PruebaServiceImpl.java
+++ src/main/java/com/lombok/service/impl/PruebaServiceImpl.java
@@ -11,26 +11,18 @@ import java.time.LocalDateTime;
 @Service
 public class PruebaServiceImpl implements PruebaService {
 
-	//Puedes hacerlo de 2 maneras.
-	/*Inicio Opcion_1
-     @Autowired PruebaRepository pruebaRepository;
-	 Fin Opcion_ */
-
-	//La segunda es así:
-	//Inicio Opcion_
 	private final PruebaRepository pruebaRepository;
 
 	@Autowired
 	public PruebaServiceImpl(PruebaRepository pruebaRepository) {
 		this.pruebaRepository = pruebaRepository;
 	}
-	//Fin Opcion_2
 
 	@Override
 	public PruebaModel savePrueba(PruebaModel pruebaModel){
 
 		if(pruebaModel == null){
-			throw new IllegalArgumentException("PruebaModel no puede ser null");
+			throw new IllegalArgumentException("Prueba Model no puede ser null");
 		}
 		try {
 			pruebaModel.setFechaRegistro(LocalDateTime.now());
